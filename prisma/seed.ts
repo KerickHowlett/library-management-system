724dbb09e812ef5bd4a77b87eb655edd49162940
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-03-29 15:49:00 -0400

    style: apply auto formatting and lint fixes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,25 @@
 import type { Faker } from '@faker-js/faker';
-import { TransactionAction, type Book, type Prisma, type PrismaClient, type User } from '@prisma/client';
+import {
+    TransactionAction,
+    type Book,
+    type Prisma,
+    type PrismaClient,
+    type User,
+} from '@prisma/client';
 import type { DefaultArgs } from '@prisma/client/runtime/library';
 
 type Dependencies = {
     faker: Faker;
-    prisma: Omit<PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>, "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends">;
-}
+    prisma: Omit<
+        PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>,
+        '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'
+    >;
+};
 
 type BookAndUser = {
-    userId: User['id'],
+    userId: User['id'];
     bookId: Book['id'];
-}
+};
 
 function pluckIDs<T extends Record<'id', any>>(ids: T[]): T['id'][] {
     return ids.map(({ id }) => id);
@@ -21,7 +30,7 @@ async function seedCheckout({ prisma, userId, bookId }: SeedCheckoutParams): Pro
     await prisma.book.update({
         data: { userId },
         where: { id: bookId },
-    })
+    });
 }
 
 async function seedTransaction({ prisma, userId, bookId }: SeedCheckoutParams): Promise<void> {
@@ -30,8 +39,8 @@ async function seedTransaction({ prisma, userId, bookId }: SeedCheckoutParams): 
             bookId,
             userId,
             action: TransactionAction.CheckedOut,
-        }
-    })
+        },
+    });
 }
 
 async function seedUsers({ faker, prisma }: Dependencies): Promise<User['id'][]> {
@@ -57,10 +66,12 @@ async function seedUsers({ faker, prisma }: Dependencies): Promise<User['id'][]>
 async function seedBooks({ faker, prisma }: Dependencies): Promise<Book['id'][]> {
     console.info('Seeding Book Data...');
 
-    const books: Prisma.BookCreateInput[] = []
+    const books: Prisma.BookCreateInput[] = [];
     for (let i = 0; i <= 20; i++) {
         const series = faker.datatype.boolean() ? faker.book.series() : undefined;
-        const edition = faker.datatype.boolean() ? faker.number.int({ min: 1, max: 20 }).toString() : null;
+        const edition = faker.datatype.boolean()
+            ? faker.number.int({ min: 1, max: 20 }).toString()
+            : null;
 
         books.push({
             series,
