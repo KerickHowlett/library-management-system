6ac1c2825b65ab871e009191e79c44e76779a542
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-03-29 23:23:57 -0400

    feat(api): create books controller

--- apps/api/src/books/books.controller.spec.ts
+++ apps/api/src/books/books.controller.spec.ts
@@ -1,14 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import type { Book } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
 import { BooksController } from './books.controller';
 import { BooksService } from './services/books.service';
+import { createBookFixture } from '../tests/utils';
+import type { CreateBookDto, UpdateBookDto } from './dto';
+
+class MockBooksService {
+    async findAll(): Promise<Book[]> {
+        return [
+            {
+                id: faker.string.uuid(),
+                createdAt: new Date(),
+                updatedAt: null,
+                userId: null,
+                ...createBookFixture(),
+            } as Book,
+        ];
+    }
+
+    async findOne(id: Book['id']): Promise<Book> {
+        return {
+            id,
+            createdAt: new Date(),
+            updatedAt: null,
+            userId: null,
+            ...createBookFixture(),
+        } as Book;
+    }
+
+    async create(dto: CreateBookDto): Promise<Book> {
+        return {
+            id: faker.string.uuid(),
+            createdAt: new Date(),
+            updatedAt: null,
+            userId: null,
+            ...dto,
+        } as Book;
+    }
+
+    async update(id: Book['id'], dto: UpdateBookDto): Promise<Book> {
+        return {
+            id,
+            createdAt: new Date(),
+            updatedAt: null,
+            userId: null,
+            ...dto,
+        } as Book;
+    }
+
+    async remove(_id: Book['id']): Promise<void> {
+        return;
+    }
+}
 
 describe('BooksController', () => {
     let controller: BooksController;
+    let mockBooksService: MockBooksService;
 
     beforeEach(async () => {
+        mockBooksService = new MockBooksService();
+
         const module: TestingModule = await Test.createTestingModule({
             controllers: [BooksController],
-            providers: [BooksService],
+            providers: [
+                {
+                    provide: BooksService,
+                    useValue: mockBooksService,
+                },
+            ],
         }).compile();
 
         controller = module.get<BooksController>(BooksController);
@@ -16,5 +77,41 @@ describe('BooksController', () => {
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    it('should return all books', async () => {
+        const books = await controller.getBooks();
+        expect(books.length).toEqual(1);
+    });
+
+    it('should return a single book by ID', async () => {
+        const id = faker.string.uuid();
+        const book = await controller.getBookById(id);
+
+        expect(book.id).toEqual(id);
+    });
+
+    it('should create a new book', async () => {
+        const dto = createBookFixture() as CreateBookDto;
+        const book = await controller.createBook(dto);
+
+        expect(book).toMatchObject(dto);
+    });
+
+    it('should update a book by ID', async () => {
+        const id = faker.string.uuid();
+        const dto = { title: 'Updated Book' } as UpdateBookDto;
+        const updatedBook = await controller.updateBook(id, dto);
+
+        expect(updatedBook).toMatchObject({ ...dto, id });
+    });
+
+    it('should delete a book by ID', async () => {
+        const removeBookSpy = jest.spyOn(mockBooksService, 'remove').mockResolvedValueOnce();
+
+        const id = faker.string.uuid();
+        await controller.deleteBook(id);
+
+        expect(removeBookSpy).toHaveBeenCalledWith(id);
     });
 });
--- apps/api/src/books/books.controller.ts
+++ apps/api/src/books/books.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 import { BooksService } from './services/books.service';
+import type { Book } from '@prisma/client';
+import type { CreateBookDto, UpdateBookDto } from './dto';
 
 @Controller('books')
 export class BooksController {
@@ -8,5 +10,25 @@ export class BooksController {
     @Get()
     getBooks() {
         return this.booksService.findAll();
+    }
+
+    @Get(':id')
+    getBookById(@Param('id') id: Book['id']) {
+        return this.booksService.findOne(id);
+    }
+
+    @Post()
+    createBook(@Body() dto: CreateBookDto) {
+        return this.booksService.create(dto);
+    }
+
+    @Put(':id')
+    updateBook(@Param('id') id: Book['id'], @Body() dto: UpdateBookDto) {
+        return this.booksService.update(id, dto);
+    }
+
+    @Delete(':id')
+    deleteBook(@Param('id') id: Book['id']) {
+        return this.booksService.remove(id);
     }
 }
