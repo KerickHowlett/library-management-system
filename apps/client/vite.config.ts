ff14c74cf99ecaec4ca780e553126530952576e6
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-03-30 19:57:49 -0400

    feat(client): add api proxy

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,16 +4,27 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
+const { PORT, HOST } = process.env;
+
+const PROXY = {
+    '/api': {
+        target: `http://${HOST}:${PORT}`,
+        rewrite: (path: string) => path.replace(/^\/api/, ''),
+    },
+};
+
 export default defineConfig(() => ({
     root: __dirname,
     cacheDir: '../../node_modules/.vite/client',
     server: {
         port: 4200,
         host: 'localhost',
+        proxy: PROXY,
     },
     preview: {
         port: 4300,
         host: 'localhost',
+        proxy: PROXY,
     },
     plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
     // Uncomment this if you are using workers.
