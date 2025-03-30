85099231874c82e263d4a84172700f0205b6be33
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-03-29 22:06:50 -0400

    feat(api): define dto and add api validation

--- apps/api/src/books/dto/create-book.dto.ts
+++ apps/api/src/books/dto/create-book.dto.ts
@@ -1,1 +1,35 @@
-export class CreateBookDto {}
+import { IsNotEmpty, IsString, IsOptional, IsEmail } from 'class-validator';
+
+export class CreateBookDto {
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsString()
+    @IsOptional()
+    edition?: string | null | undefined;
+
+    @IsString()
+    @IsNotEmpty()
+    author: string;
+
+    @IsString()
+    @IsNotEmpty()
+    genre: string;
+
+    @IsString()
+    @IsOptional()
+    series?: string | null | undefined;
+
+    @IsString()
+    @IsNotEmpty()
+    publisher: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    fullName: string;
+}
