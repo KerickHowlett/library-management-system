6030a7a0f9932a71550ebc2c90fb174eccccb5c0
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-03-29 23:25:39 -0400

    feat(api): add books repository to module

--- apps/api/src/books/books.module.ts
+++ apps/api/src/books/books.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+
 import { BooksService } from './services/books.service';
 import { BooksController } from './books.controller';
+import { BooksPrismaRepository } from './repositories/books.prisma.respository';
 
 @Module({
     controllers: [BooksController],
-    providers: [BooksService],
+    providers: [
+        BooksService,
+        {
+            provide: 'BOOK_REPOSITORY',
+            useValue: BooksPrismaRepository,
+        },
+    ],
 })
 export class BooksModule {}
