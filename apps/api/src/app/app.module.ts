2bd7fdcb68340580223db3fcd701ad559ae2da71
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-03-29 21:28:47 -0400

    refactor(api): refine prisma service

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaModule } from '../prisma/prisma.module';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from '../books/books.module';
 
 @Module({
-    imports: [PrismaModule, BooksModule],
+    imports: [BooksModule],
     controllers: [AppController],
     providers: [AppService],
 })
--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,19 +1,8 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable, OnModuleDestroy, OnModuleInit, Scope } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-    constructor(config: ConfigService) {
-        super({
-            datasources: {
-                db: {
-                    url: config.get('DATABASE_URL'),
-                },
-            },
-        });
-    }
-
     async onModuleInit() {
         await this.$connect();
     }
